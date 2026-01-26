181adab9c4209e8b2492c8ba1d94dac24ee7d0df
Amartya Raybo Ghosh <arayboghosh@gmail.com>
2026-01-26 13:48:17 -0800

    Updates with interfaces

--- src/main/java/edu/ucsd/spendingtracker/model/Expense.java
+++ src/main/java/edu/ucsd/spendingtracker/model/Expense.java
@@ -4,11 +4,19 @@ public class Expense {
     private String name;
     private Category category;
     private double amount;
+    private int id;
 
+    public Expense(String name, Category category, double amount, int id) {
+        this.name = name;
+        this.category = category;
+        this.amount = amount;
+        this.id = id;
+    }
     public Expense(String name, Category category, double amount) {
         this.name = name;
         this.category = category;
         this.amount = amount;
+        this.id = -1;
     }
 
     public String getName() {
@@ -21,6 +29,10 @@ public class Expense {
 
     public double getAmount() {
         return amount;
+    }
+
+    public int getId() {
+        return id;
     }
 }
 
